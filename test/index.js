df61e0e83d3fdbb8aff2d6eac311f07ba79f9de0
Eran Hammer <eran@hammer.io>
2019-08-24 04:18:46 +0000

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Path = require('path');
+
+const Code = require('@hapi/code');
+const Lab = require('@hapi/lab');
+const Pinpoint = require('..');
+
+
+const internals = {};
+
+
+const { describe, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+
+describe('Pinpoint', () => {
+
+    describe('location()', () => {
+
+        it('returns current location', () => {
+
+            expect(Pinpoint.location()).to.equal({
+                filename: Path.join(__dirname, 'index.js'),
+                line: 23
+            });
+        });
+
+        it('returns location or upper caller', () => {
+
+            expect(Pinpoint.location(2).filename).to.contain('@hapi/lab');
+        });
+    });
+});
---
+++ test/index.ts
@@ -0,0 +1,18 @@
+import * as Pinpoint from '..';
+import * as Lab from '@hapi/lab';
+
+
+const { expect } = Lab.types;
+
+
+// location()
+
+Pinpoint.location();
+Pinpoint.location(1);
+
+expect.type<Pinpoint.deepEqual.Location>(Pinpoint.location());
+expect.type<string>(Pinpoint.location().filename);
+expect.type<number>(Pinpoint.location().line);
+
+expect.error(Pinpoint.location(1, 2));
+expect.error(Pinpoint.location('1'));
